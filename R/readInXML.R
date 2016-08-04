ccd9da079ecc64e6258321f54f3c2bfaafabbeec
jhamburg <jhamburg88@gmail.com>
2016-08-03 22:12:28 -0400

    added code to rename list with the name of the file

--- R/readInXML.R
+++ R/readInXML.R
@@ -7,11 +7,14 @@ library(XML)
 readInXMLs <- function(yr){
   dir <- file.path('data', 'raw', yr)
   files <- list.files(dir)
+  fnms <- substr(files, 1, regexpr("\\.xml", files) - 1)
+  updFnms <- updVarNames(fnms)
   
   xmlReadFunc <- function(x) xmlToDataFrame(file.path(dir, x))
   
-  aa <- lapply(files, xmlReadFunc)
-  return(aa)
+  raw <- lapply(files, xmlReadFunc)
+  names(raw) <- updFnms
+  return(raw)
 }
 
 aa <- readInXMLs(2013)
